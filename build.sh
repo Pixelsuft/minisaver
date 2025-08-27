0036c9710701a0bd760583810fded8c6cc23de80
pixelsuft‮ <68371847+Pixelsuft@users.noreply.github.com>
2025-08-27 17:48:30 +0700

    Update build.sh for Win32

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-# For Win32: cc main.c -lSDL3 -ldwmapi $CFLAGS $LDFLAGS -Wextra -Wpedantic -o minisaver.exe
+# For Win32: cc main.c -lSDL3 -ldwmapi $CFLAGS $LDFLAGS -Wextra -Wpedantic -mwindows -o minisaver.exe
 cc main.c -lSDL3 $CFLAGS $LDFLAGS -Wextra -Wpedantic -o minisaver
